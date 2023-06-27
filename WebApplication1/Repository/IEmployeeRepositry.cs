c400f7ad9f583516277cb8556a5a2b56e8611dd9
AhmedAboAlata1 <62518605+ahmedAboalata1@users.noreply.github.com>
2023-06-27 11:21:10 +0300

    working on

--- WebApplication1/Repository/IEmployeeRepositry.cs
+++ WebApplication1/Repository/IEmployeeRepositry.cs
@@ -8,7 +8,6 @@ namespace HRSystem.Repository
         public List<Employee> getAll();
         public List<Employee> getByName(string name);
         public void add(Employee employee);
-
         public void update(Employee employee);
         public void delete(int id);
         public void save();
