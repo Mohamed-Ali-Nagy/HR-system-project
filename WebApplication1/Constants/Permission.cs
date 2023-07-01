0d3b26652591a6f4d6ae0f4b1280c6e80da96e72
Mamn847 <133536689+Mamn847@users.noreply.github.com>
2023-07-01 17:30:48 +0300

    permissins

--- WebApplication1/Constants/Permission.cs
+++ WebApplication1/Constants/Permission.cs
@@ -39,7 +39,27 @@
             public const string Edit = "Permission.Attendance.Edit";
             public const string Delete = "Permission.Attendance.Delete";
         }
-
+        public static class GeneralSetting
+        {
+            public const string View = "Permission.GeneralSetting.View";
+            public const string Create = "Permission.GeneralSetting.Create";
+            public const string Edit = "Permission.GeneralSetting.Edit";
+            public const string Delete = "Permission.GeneralSetting.Delete";
+        }
+        public static class Holidays
+        {
+            public const string View = "Permission.Holidays.View";
+            public const string Create = "Permission.Holidays.Create";
+            public const string Edit = "Permission.Holidays.Edit";
+            public const string Delete = "Permission.Holidays.Delete";
+        }
+        public static class SalaryReport
+        {
+            public const string View = "Permission.SalaryReport.View";
+            public const string Create = "Permission.SalaryReport.Create";
+            public const string Edit = "Permission.SalaryReport.Edit";
+            public const string Delete = "Permission.SalaryReport.Delete";
+        }
 
     }
 }
